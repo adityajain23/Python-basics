30c19aef2f9a23648979ebec30a7a95bdfb6be34
adityajain23 <65896927+adityajain23@users.noreply.github.com>
2021-03-29 23:33:24 +0530

    Add files via upload

---
+++ visualization_libraries.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd 
+
+st.title("Which python visualization library is best suited for your project")
+st.markdown(
+	"* Matplotlib\n* Plotly\n* Seaborn\n* GGplot\n* Altair\n"
+	"* Bokeh\n* Pygal\n* Geoplotlib\n* Gleam\n* Missingno\n* Leather"
+	)
