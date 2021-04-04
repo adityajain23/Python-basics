f050adeb60e8c84e712527275d9ea985a76ce115
adityajain23 <65896927+adityajain23@users.noreply.github.com>
2021-04-04 18:15:21 +0530

    Update visualization_libraries.py

--- visualization_libraries.py
+++ visualization_libraries.py
@@ -2,8 +2,47 @@ import streamlit as st
 import numpy as np
 import pandas as pd 
 
-st.title("Which python visualization library is best suited for your project")
+st.markdown("<h1 style='text-align: center;'>Which python visualization library is best suited for your project</h1>", unsafe_allow_html=True)
 st.markdown(
 	"* Matplotlib\n* Plotly\n* Seaborn\n* GGplot\n* Altair\n"
 	"* Bokeh\n* Pygal\n* Geoplotlib\n* Gleam\n* Missingno\n* Leather"
 	)
+st.markdown("<h1 style='text-align: center;'>Matplotlib</h1>", unsafe_allow_html=True)
+st.markdown('''<ol>
+  <li style='font-size: 20px;'>What is Matplotlib?</li>
+  <li style='font-size: 20px;'>Why use Matplotlib?</li>
+  <li style='font-size: 20px;'>What functionalities does it provide?</li>
+  <li style='font-size: 20px;'>When to use it?</li>
+  <li style='font-size: 20px;'>Unique features of Matplotlib over others.</li>
+</ol>''', unsafe_allow_html=True)	
+
+st.markdown('<hr style="height:2px;border-width:0;color:gray;background-color:gray"></hr>', unsafe_allow_html=True)
+st.markdown("<p style='font-size: 24px;'>⇨ What is Matplotlib?</p>", unsafe_allow_html=True)
+
+st.markdown('''<ul>
+	<li style='font-size: 16px;'>Matplotlib is one of the most popular Python packages used for data visualization.</li>
+	<li style='font-size: 16px;'>It is a cross-platform library for making 2D plots from data in arrays.</li>
+	<li style='font-size: 16px;'>It can be used in Python and IPython shells, Jupyter notebook, and web application servers also.</li>
+	</ul>''', unsafe_allow_html=True)
+
+st.markdown('<hr style="height:2px;border-width:0;color:gray;background-color:gray"></hr>', unsafe_allow_html=True)
+st.markdown("<p style='font-size: 24px;'>⇨ Why use Matplotlib?</p>", unsafe_allow_html=True)
+
+st.markdown('''<ul>
+	<li style='font-size: 16px;'>Matploitlib is a Python library used for creating static,
+	animated, and interactive visualizations in Python, this python library provides
+	and objected-oriented APIs for integrating plots into applications.</li>
+	</ul>''', unsafe_allow_html=True)
+
+st.markdown('<hr style="height:2px;border-width:0;color:gray;background-color:gray"></hr>', unsafe_allow_html=True)
+st.markdown("<p style='font-size: 24px;'>⇨ What functionalities does it provide?</p>", unsafe_allow_html=True)
+
+st.markdown('''<ul>
+	<li style='font-size: 16px;'>One of the greatest benefits of visualization is that it
+	allows us visual access to huge amounts of data in easily digestible visuals. 
+	Matplotlib consists of several plots like line, bar, scatter, histogram, etc. Plots 
+	help to understand trends, patterns, and to make correlations. They’re typically 
+	instruments for reasoning about quantitative information. Some of the sample plots are covered here.</li>
+	</ul>''', unsafe_allow_html=True)
+
+st.markdown('<hr style="height:2px;border-width:0;color:gray;background-color:gray"></hr>', unsafe_allow_html=True)
